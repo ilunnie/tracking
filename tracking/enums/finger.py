ae0f88d69d68f864350b1b869ac870956d163670
ilunnie <luisgustavocaris2004@gmail.com>
2024-09-17 17:00:26 -0300

    feature: constant indexes relative to the fingers

--- tracking/enums/finger.py
+++ tracking/enums/finger.py
@@ -1,8 +1,25 @@
 from enum import Enum
 
+from ..hand_tracking.constants import HandIndexes
+
 class FingerEnum(Enum):
     THUMB   = 0
     INDEX   = 1
     MIDDLE  = 2
     RING    = 3
     PINKY   = 4
+
+    @classmethod
+    def get_tip(cls, finger: 'FingerEnum') -> int:
+        name = finger.name if finger == cls.THUMB else finger.name + "_FINGER"
+        return HandIndexes[f"HAND_{name}_TIP"].value
+
+    @classmethod
+    def get_points(cls, finger: 'FingerEnum') -> set:
+        name = finger.name if finger == cls.THUMB else finger.name + "_FINGER"
+        return HandIndexes[f"HAND_{name}"].value
+    
+    @classmethod
+    def get_connections(cls, finger: 'FingerEnum') -> tuple:
+        name = finger.name if finger == cls.THUMB else finger.name + "_FINGER"
+        return  HandIndexes[f"HAND_{name}_CONNECTIONS"].value
