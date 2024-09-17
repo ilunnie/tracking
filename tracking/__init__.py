fe6d93f5dea1ad7787b317a03f56a6f18296c233
ilunnie <luisgustavocaris2004@gmail.com>
2024-09-17 15:59:38 -0300

    feature: webcam class

--- tracking/__init__.py
+++ tracking/__init__.py
@@ -1,15 +1,20 @@
+import cv2
+
 from typing import Union, Tuple, List
 
 from . import exceptions
 
 from .constants import CONFIG
 from .enums import TypeEnum as type
+from .webcam import WebCam
 
 def init(
         screen_size: Union[Tuple[int, int], List[int]],
+        video_capture: Union[cv2.VideoCapture, int] = 0,
         flags: List[Union[int, type]] = 0
 ):
     CONFIG.SCREEN_WIDTH, CONFIG.SCREEN_HEIGHT = screen_size
+    CONFIG.VIDEO_CAPTURE = WebCam(video_capture)
     
     for flag in [flags] if isinstance(flags, (int, type)) else set(flags):
         value = flag.value if isinstance(flag, type) else flag
