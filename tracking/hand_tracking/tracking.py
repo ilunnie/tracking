80845bc7298159682ab5e6bdee7f515ed7f58ad8
ilunnie <luisgustavocaris2004@gmail.com>
2024-09-23 14:01:18 -0300

    feature: now hand tracking callback receive list<Hand>

--- tracking/hand_tracking/tracking.py
+++ tracking/hand_tracking/tracking.py
@@ -23,8 +23,8 @@ class Tracking:
                  min_tracking_confidence: float = 0.5,
                  result_callback: Optional[
                      Callable[[
-                         HandLandmarkerResult,
-                         np.ndarray,
+                         List[Hand],
+                         mp.Image,
                          int], None]] = None) -> None:
         
         base_options = BaseOptions(model_asset_path='./tasks/hand_landmarker.task')
@@ -46,54 +46,56 @@ class Tracking:
                  result: HandLandmarkerResult, 
                  image: mp.Image, 
                  timestamp: int):
-        Result = namedtuple('hand', ['classification', 'landmarks', 'image'])
-        self.__result = [Result(classification=hand[0].display_name,
+        self.__result = result
+
+        ResultTuple = namedtuple('hand', ['classification', 'landmarks', 'image'])
+        results = [ResultTuple(classification=hand[0].display_name,
                                 landmarks=marks,
-                                image=self.__image)
+                                image=image)
                         for hand, marks
                         in zip(result.handedness, result.hand_landmarks)]
-        
-        self.__callback.set()
-        if self.__result_callback:
-            self.__result_callback(result, self.__image, timestamp)
 
-    def predict(self, image: Optional[np.ndarray] = None, side_mirror = False) -> List[Hand]:
-        results = self.process(image)
-        
-        hands = [Hand((SideEnum.mirror(SideEnum.from_string(hand))
+        side_mirror = self.__side_mirror
+        self.__hands = [Hand((SideEnum.mirror(SideEnum.from_string(hand))
                        if side_mirror
                        else SideEnum.from_string(hand)),
-                      Landmarks(image, marks))
+                      Landmarks(image.numpy_view(), marks))
                  for hand, marks, image
                  in results]
         
-        return hands
-        
-    def process(self, image: Optional[np.ndarray] = None) -> List[NamedTuple]:
+        self.__callback.set()
+        if self.__result_callback:
+            self.__result_callback(self.__hands, image, timestamp)
+
+    def predict(self, image: Optional[np.ndarray] = None, side_mirror = False) -> List[Hand]:
+        self.__side_mirror = side_mirror
         if image is None:
             _, image = CONFIG.VIDEO_CAPTURE.read()
+        
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-        self.__image = image
+        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
 
+        self.detect(image)
+        return self.__hands
+    
+    def detect(self, image: mp.Image) -> HandLandmarkerResult:
         self.__callback.clear()
         if self.__running_mode == RunningModeEnum.LIVE_STREAM:
+            now = datetime.now()
             if self.__lastupdate:
-                now = datetime.now()
                 self.__timestamp += int((now - self.__lastupdate).total_seconds() * 1000)
                 self.__lastupdate = now
             else:
                 self.__timestamp = 0
-                self.__lastupdate = datetime.now()
-            self.handsmesh.detect_async(mp_image, self.__timestamp)
+                self.__lastupdate = now
+            self.handsmesh.detect_async(image, self.__timestamp)
         else:
             if self.__running_mode == RunningModeEnum.IMAGE:
-                result = self.handsmesh.detect(mp_image)
+                result = self.handsmesh.detect(image)
             else:
                 self.__timestamp += 1
-                result = self.handsmesh.detect_for_video(mp_image, self.__timestamp)
-                
-            self.callback(result, mp_image, self.__timestamp)
-            
+                result = self.handsmesh.detect_for_video(image, self.__timestamp)
+            self.callback(result, image, self.__timestamp)
+
         self.__callback.wait()
         return self.__result
